df184186345175afd115017b14bd5daaa2b52bf1
ppl8763 <priyansu8763@gmail.com>
2026-01-15 16:16:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import socketio
+from fastapi import FastAPI
+
+sio = socketio.AsyncServer(
+    async_mode="asgi",
+    cors_allowed_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://z1vmgmts-5173.inc1.devtunnels.ms" ,
+    ]
+)
+
+
+app = FastAPI()
+socket_app = socketio.ASGIApp(
+    sio,
+    other_asgi_app=app,
+    socketio_path="socket.io"
+)
+
+users = {}
+
+@sio.event
+async def connect(sid, environ):
+    print("✅ Connected:", sid)
+
+@sio.event
+async def join(sid, username):
+    users[sid] = username
+    await sio.emit("users", list(users.values()))
+
+@sio.event
+async def chat_message(sid, msg):
+    await sio.emit(
+        "chat_message",
+        {
+            "user": users.get(sid),
+            "message": msg
+        }
+    )
+
+@sio.event
+async def disconnect(sid):
+    users.pop(sid, None)
+    await sio.emit("users", list(users.values()))
+    print("❌ Disconnected:", sid)
+
+@app.get("/health")
+async def root():
+    return {"status": "ok"}
